138489b1739098c5ca52d7b5c64375fce7f495d1
eskin22 <blakepmcbride@gmail.com>
2023-04-24 17:02:59 -0500

    player class modifications

---
+++ testing/testing_player.py
@@ -0,0 +1,21 @@
+
+# append sys path to access the class files
+import sys, os
+sys.path.append('../EpicQuest')
+
+# import player class
+from classes.player import Player
+
+# create player object
+player = Player("Chad", "Fuck")
+
+# define basic enemy class
+class Enemy():
+    def __init__(self, name, hp):
+        self.name = name
+        self.hp = hp
+
+# create enemy object 
+goblin = Enemy("Goblin", 25)
+
+player.attack(goblin)
